2563ef6a869af7f2f2194113e8da46dd9d5ad971
Dmitry Kovalev <trigger1610@gmail.com>
2025-04-09 22:27:32 +0400

    Init project

---
+++ shared/src/commonMain/kotlin/dmko/guitarbeat/Greeting.kt
@@ -0,0 +1,9 @@
+package dmko.guitarbeat
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/dmko/guitarbeat/Platform.kt
@@ -0,0 +1,7 @@
+package dmko.guitarbeat
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
